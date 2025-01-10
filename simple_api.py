85ae97495f1eac3fbda881b6055c7d90386bb66b
大村俊輔 <oomurashunsuke@oomurashunsukenoMacBook-Air.local>
2025-01-10 14:36:28 +0900

    Update API to return random number between 1 and 50 and adjust tests

--- simple_api.py
+++ simple_api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/api/fixed', methods=['GET'])
 def fixed_value():
     import random
-    random_number = random.randint(1, 100)
+    random_number = random.randint(1, 50)
     return jsonify({"random_number": random_number})
 
 if __name__ == '__main__':
